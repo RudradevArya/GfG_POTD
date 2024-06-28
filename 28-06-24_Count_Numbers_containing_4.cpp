b11cd1de2cf4e0d2558fb60b4581917548d6a76d
Rudradev <rudradev93@gmail.com>
2024-06-28 23:47:21 +0530

    12 June 24 GfG POTD solution

---
+++ 28-06-24_Count_Numbers_containing_4.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    int countNumberswith4(int n) {
+        // code here
+        int cnt = 0;
+        for(int i=1; i<=n; ++i)
+        {
+            if(help(i))
+            {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+    
+    bool help(int n)
+    {
+        while(n > 0)
+        {
+            if(n % 10 == 4) return true;
+            n /= 10;
+        }
+        return false;
+    }
+    
+};
